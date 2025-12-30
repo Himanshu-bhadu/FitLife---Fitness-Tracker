86f93b85b5b0faccff8cef483c9af4a211c36f1c
Himanshu-bhadu <himanshubhadu103@gmail.com>
2025-12-30 22:52:30 +0530

    reset email connection

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://fit-life-fitness-tracker.vercel.app/', 
   credentials: true
 }));
 app.use(express.json());
